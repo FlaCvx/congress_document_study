92c7a7e3302067171a3ecf1059adc110e9713933
Flavia Cavallaro <flavia@cnb-d102-62.inf.ethz.ch>
2019-12-08 15:16:25 +0100

    add scr

---
+++ data/count_files.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+default_path_one="1789to1824_DebatesAndProceedings"
+default_path_two="1824to1837_DebatesAndProceedings"
+
+dir_one='volumes'
+dir_two='items'
+
+subdir_one='Volume_'
+subdir_two='Item_'
+
+end_one=42
+end_two=29
+
+default_path=${default_path_two}
+dir=${dir_two}
+subdir=${subdir_two}
+end=${end_two}
+
+
+echo "Main path: ${default_path}"
+for i in `seq 1 1 ${end}`; do
+
+  c=$(find ${default_path}/${dir}/${subdir}${i} -type f -name 'page*' | wc -l)
+  echo "Pages in /${dir}/${subdir}${i}: ${c}"
+  sum=$((sum+c))
+
+  c=$(find ${default_path}/hocrs_files/${subdir}${i} -type f -name 'page*' | wc -l)
+  echo "Pages in /hocrs_files/${subdir}${i}: ${c}"
+  sum=$((sum+c))
+
+  c=$(find ${default_path}/one_column_oriented/${subdir}${i} -type f -name 'page*' | wc -l)
+  echo "Pages in /one_column_oriented/${subdir}${i}: ${c}"
+  sum=$((sum+c))
+
+  c=$(find ${default_path}/text_volumes/${subdir}${i} -type f -name 'page*' | wc -l)
+  echo "Pages in /text_volumes/${subdir}${i}: ${c}, ${c}x4: $(($c * 4))"
+  sum=$((sum+c))
+
+  c=$(find ${default_path}/speeches/${subdir}${i} -type f -name 'page*' | wc -l)
+  echo "Pages in /speeches/${subdir}${i}: ${c}"
+  sum=$((sum+c))
+
+  echo ""
+
+done
+
+
+echo $sum
