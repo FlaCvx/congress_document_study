c7ffd65aac1d63bb518f73d24f860dc7ce615398
cflavia <cflavia@student.ethz.ch>
2020-01-13 11:15:33 +0100

    .

--- data/bigram_analysis.py
+++ data/bigram_analysis.py
@@ -363,9 +363,8 @@ def match_congressmen(bigrams_count, df_congressmen):
 
     #Filter only the columns of people in the df_congressmen dataframe, this will make the merge easier.
     columns_congressmen = bigrams_count.columns.difference(["w0","w1"]).values
-    matches = [man for man in columns_congressmen if man.split("mr ")[-1] in df_congressmen.match_surname]
+    matches = [man for man in columns_congressmen if man.split("mr ")[-1] in df_congressmen.match_surname.values]
     matches = matches + ["w0","w1"]
-    print(f"Len of matches: {len(matches)}")
     bigrams_count = bigrams_count[matches]
 
     #Create the match_surname column for te bigrmas dataframe as well
